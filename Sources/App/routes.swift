696c86f80566056cc2df5058099d38d698f9b381
Yi donghoon <icq4ever@gmail.com>
2021-07-21 14:33:28 +0900

    initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,12 +2,25 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    // index
     app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+        return req.view.render("index", ["title": "Unbarrier"])
     }
 
     app.get("hello") { req -> String in
         return "Hello, world!"
+    }
+
+    app.get("app") {req in
+        return req.view.render("app", ["title":"Unbarrier Recorder"])
+    }
+    
+    app.get("app", "login") { req in
+        return req.view.render("login", ["title": "login"])
+    }
+
+    app.get("app", "signup") { req in
+        return req.view.render("signup", ["title": "sign up"])
     }
 
     try app.register(collection: TodoController())
